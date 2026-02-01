aef1ba6c1f7a4987c9dcca8aa70d8384324ff39c
atharva2610 <atharvavishwakarma526@gmail.com>
2026-02-01 21:40:09 +0530

    Added fund account transaction filter, displaying total credit and debit amount for transactions and generating monthly reports data with csv download.

--- app_expenses/views/__init__.py
+++ app_expenses/views/__init__.py
@@ -3,3 +3,4 @@ from .fund_account_view import fund_accounts, create_fund_account, update_fund_a
 from .category_view import categories, create_category, update_category
 from .tag_view import tags, create_tag, update_tag
 from .transaction_view import transactions, create_transaction, update_transaction
+from .report_view import report, update_report
